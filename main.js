7021b65813a86bff145837349956ff4c70148a83
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-06-23 12:26:28 -0400

    Implement full functionality

--- main.js
+++ main.js
@@ -11,21 +11,24 @@ function addBookToLibrary(title, author, numPages, read) {
     myLibrary.push(new Book(title, author, numPages, read));
 }
 
-function toggleReadStatus(book) {
-    book.read = !book.read;
-}
-
-// Manually added books
-addBookToLibrary("Harry Potter", "JK Rowling", "295", false);
-addBookToLibrary("A Fault in Our Stars", "John Blue", "295", true);
-
-const table = document.querySelector("#main");
-myLibrary.forEach((book) => {
+function outputBookToTable(book) {
     const row = document.createElement("tr");
     const propertyValues = Object.values(book);
     propertyValues.forEach((attribute) => {
         const data = document.createElement("td");
-        data.textContent = attribute;
+        if (typeof attribute === "boolean") {
+            const readCheckBox = document.createElement("input");
+            readCheckBox.setAttribute("type", "checkbox");
+            if (book.read) {
+                readCheckBox.checked = true;
+            }
+            data.appendChild(readCheckBox);
+            readCheckBox.addEventListener("change", () => {
+                toggleReadStatus(book);
+            });
+        } else {
+            data.textContent = attribute;
+        }
         row.appendChild(data);
     });
     const deleteContainer = document.createElement("td");
@@ -40,12 +43,29 @@ myLibrary.forEach((book) => {
     })
 
     table.appendChild(row);
+}
+
+function toggleReadStatus(book) {
+    book.read = !book.read;
+}
+
+// Manually added books
+addBookToLibrary("Harry Potter", "JK Rowling", "295", false);
+addBookToLibrary("A Fault in Our Stars", "John Blue", "295", true);
+
+const table = document.querySelector("#main");
+myLibrary.forEach((book) => {
+    outputBookToTable(book);
 });
 
 const newBookBtn = document.querySelector("#new-button");
 const cancelBtn = document.querySelector("#cancel-button");
 const addBookBtn = document.querySelector("#add-button");
 const newBookDialog = document.querySelector("#new-dialog");
+
+const titleField = document.querySelector("#title");
+const authorField = document.querySelector("#author");
+const pagesField = document.querySelector("#pages");
 
 newBookBtn.addEventListener("click", () => {
     newBookDialog.showModal();
@@ -57,6 +77,9 @@ cancelBtn.addEventListener("click", (e) => {
 });
 
 addBookBtn.addEventListener("click", (e) => {
-    myLibrary.push(new Book(title, author, numPages, read));
+    const selectedReadStatus = document.querySelector("input[name='read_status']:checked");
+    const readStatus = selectedReadStatus.value === "yes";
+    addBookToLibrary(titleField.value, author.value, pagesField.value, readStatus);
+    outputBookToTable(myLibrary[myLibrary.length - 1]);
     e.preventDefault();
 });
